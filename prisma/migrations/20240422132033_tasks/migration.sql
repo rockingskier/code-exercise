b7c8ac8eec4bc1d436d04dad6fcf356ae0969392
Ben Delevingne <rockingskier@gmail.com>
2024-04-22 15:13:18 +0100

    fixup! feat: add Task tables to db

--- prisma/migrations/20240422132033_tasks/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "userId" TEXT NOT NULL,
-    "body" TEXT NOT NULL,
-    "completed" BOOLEAN NOT NULL,
-    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
---
+++ prisma/migrations/20240422135806_tasks/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "body" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL
+);
